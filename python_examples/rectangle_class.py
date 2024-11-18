c365e36b994094eb87dd0b58695f222955861fa2
Carlos Sergio Vaz Porto Junior <140349005+CarlosVazPorto@users.noreply.github.com>
2024-11-18 16:48:32 -0300

    Add files via upload
    Added the rectangle_class.py file.

---
+++ python_examples/rectangle_class.py
@@ -0,0 +1,19 @@
+class Rectangle():
+    def __init__(self, w, l):
+        self.width = w
+        self.length = l
+
+    def area(self):
+        return self.width * self.length
+
+    def change_size(self, w, l):
+        self.width = w
+        self.length = l
+
+
+rectangle = Rectangle(10, 20)
+print(rectangle.area())
+
+rectangle.change_size(20, 40)
+print(rectangle.area())
+
