df53789dcd47ff3235d679aba211b7552f85db9d
Carlos Sergio Vaz Porto Junior <140349005+CarlosVazPorto@users.noreply.github.com>
2024-12-14 23:47:35 -0300

    Add files via upload

---
+++ python_examples/data_structures/stack.py
@@ -0,0 +1,25 @@
+# From The Self-taught Programmer, by Cory Althoff
+#
+# Carlos Vaz Porto
+# December/2024
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        last = len(self.items) - 1
+        return self.items[last]
+    
+    def size(self):
+        return len(self.items)
+
